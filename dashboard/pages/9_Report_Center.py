76ddafb775ef8177db6e19ecae4d1642d7d4bf11
Samm-05 <samyakmahatme@gmail.com>
2026-06-13 13:26:22 +0530

    add new changes

---
+++ dashboard/pages/9_Report_Center.py
@@ -0,0 +1,39 @@
+import sys
+from pathlib import Path
+
+ROOT_DIR = (
+    Path(__file__)
+    .resolve()
+    .parent
+    .parent
+    .parent
+)
+
+sys.path.append(
+    str(ROOT_DIR)
+)
+
+import streamlit as st
+
+from src.train import train_model
+
+st.title(
+    "Report Center"
+)
+
+df, X, labels = train_model()
+
+csv = df.to_csv(
+    index=False
+)
+
+st.download_button(
+    label="Download Customer Segments CSV",
+    data=csv,
+    file_name="customer_segments.csv",
+    mime="text/csv"
+)
+
+st.dataframe(
+    df.head(20)
+)
