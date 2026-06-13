1f541e50779e1e428fc975553489221b09fefb2c
Samm-05 <samyakmahatme@gmail.com>
2026-06-13 13:02:59 +0530

    add new changes

---
+++ dashboard/Home.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Mall Customer Analytics",
+    layout="wide"
+)
+
+st.title(
+    "Mall Customer Analytics Platform"
+)
+
+st.markdown(
+    """
+    ### Features
+
+    - Customer Segmentation
+    - PCA Visualization
+    - Business Insights
+    - Customer Predictor
+    - Model Comparison
+    """
+)
